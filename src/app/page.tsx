5c7ace7027473b7a26b85247634aef924d5259ee
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2024-12-21 03:19:36 +0100

    feat: Add Opinions section to the main view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import { Header } from "./Components/MainPage/Header/Header";
+import { Opinion } from "./Components/MainPage/Opinion/Opinion";
 
 export default function Home() {
-  return;
+	return (
+		<>
+			<Header />
+			<Opinion />
+		</>
+	);
 }
